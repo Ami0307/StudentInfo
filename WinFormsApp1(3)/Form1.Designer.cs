1ecee2156c618badeaa607ddd55a7fb8f5af237d
Ami <34254387+Ami0307@users.noreply.github.com>
2024-07-11 13:07:06 +0800

    Add files via upload

---
+++ WinFormsApp1(3)/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListBox StudentsList;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox EmailBox;
+        private System.Windows.Forms.ComboBox ProgramsBox;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button ClearButton;
+
+        // Clean up any resources being used.
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // Required method for Designer support - do not modify
+        // the contents of this method with the code editor.
+        private void InitializeComponent()
+        {
+            StudentsList = new ListBox();
+            NameBox = new TextBox();
+            EmailBox = new TextBox();
+            ProgramsBox = new ComboBox();
+            SaveButton = new Button();
+            ClearButton = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // StudentsList
+            // 
+            StudentsList.FormattingEnabled = true;
+            StudentsList.Location = new Point(21, 12);
+            StudentsList.Name = "StudentsList";
+            StudentsList.Size = new Size(534, 224);
+            StudentsList.TabIndex = 0;
+            StudentsList.SelectedIndexChanged += StudentsList_SelectedIndexChanged;
+            // 
+            // NameBox
+            // 
+            NameBox.Location = new Point(78, 257);
+            NameBox.Name = "NameBox";
+            NameBox.Size = new Size(84, 27);
+            NameBox.TabIndex = 1;
+            NameBox.TextChanged += NameBox_TextChanged;
+            // 
+            // EmailBox
+            // 
+            EmailBox.Location = new Point(243, 257);
+            EmailBox.Name = "EmailBox";
+            EmailBox.Size = new Size(108, 27);
+            EmailBox.TabIndex = 2;
+            // 
+            // ProgramsBox
+            // 
+            ProgramsBox.FormattingEnabled = true;
+            ProgramsBox.Location = new Point(416, 256);
+            ProgramsBox.Name = "ProgramsBox";
+            ProgramsBox.Size = new Size(139, 28);
+            ProgramsBox.TabIndex = 3;
+            // 
+            // SaveButton
+            // 
+            SaveButton.Location = new Point(357, 308);
+            SaveButton.Name = "SaveButton";
+            SaveButton.Size = new Size(75, 23);
+            SaveButton.TabIndex = 4;
+            SaveButton.Text = "Save";
+            SaveButton.UseVisualStyleBackColor = true;
+            SaveButton.Click += SaveButton_Click;
+            // 
+            // ClearButton
+            // 
+            ClearButton.Location = new Point(168, 308);
+            ClearButton.Name = "ClearButton";
+            ClearButton.Size = new Size(75, 23);
+            ClearButton.TabIndex = 5;
+            ClearButton.Text = "Clear";
+            ClearButton.UseVisualStyleBackColor = true;
+            ClearButton.Click += ClearButton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 260);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(184, 260);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 20);
+            label2.TabIndex = 7;
+            label2.Text = "Email";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(357, 260);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 20);
+            label3.TabIndex = 8;
+            label3.Text = "Major";
+            label3.Click += label3_Click;
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(584, 361);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(ClearButton);
+            Controls.Add(SaveButton);
+            Controls.Add(ProgramsBox);
+            Controls.Add(EmailBox);
+            Controls.Add(NameBox);
+            Controls.Add(StudentsList);
+            Name = "Form1";
+            Text = "Student Management";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ WinFormsApp1(3)/Form1.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString;
+        private Student currentStudent = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string path = Directory.GetCurrentDirectory();
+            this.connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + path +
+  "\\student.mdf;Integrated Security=True;Connect Timeout=30";
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            StudentsList.Items.Clear();
+            ProgramsBox.Items.Clear();
+
+            using (SqlConnection myConnection = new SqlConnection(connectionString))
+            {
+                myConnection.Open();
+
+                // Load Students
+                string sql = "SELECT * FROM students";
+                SqlCommand myCommand = new SqlCommand(sql, myConnection);
+                SqlDataReader myReader = myCommand.ExecuteReader();
+
+                while (myReader.Read())
+                {
+                    Student student = new Student
+                    {
+                        StudentID = myReader.GetInt32(0),
+                        Name = myReader.GetString(1),
+                        Email = myReader.GetString(2),
+                        MajorID = myReader.GetInt32(3)
+                    };
+                    StudentsList.Items.Add(student);
+                }
+                myReader.Close();
+
+                // Load Majors
+                sql = "SELECT * FROM programs";
+                myCommand = new SqlCommand(sql, myConnection);
+                myReader = myCommand.ExecuteReader();
+
+                while (myReader.Read())
+                {
+                    Major major = new Major
+                    {
+                        MajorID = myReader.GetInt32(0),
+                        Name = myReader.GetString(1)
+                    };
+                    ProgramsBox.Items.Add(major);
+                }
+
+                myConnection.Close();
+            }
+        }
+
+        private void StudentsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (StudentsList.SelectedIndex != -1)
+            {
+                currentStudent = (Student)StudentsList.SelectedItem;
+                NameBox.Text = currentStudent.Name;
+                EmailBox.Text = currentStudent.Email;
+
+                for (int i = 0; i < ProgramsBox.Items.Count; i++)
+                {
+                    if (((Major)ProgramsBox.Items[i]).MajorID == currentStudent.MajorID)
+                    {
+                        ProgramsBox.SelectedIndex = i;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            currentStudent = null;
+            StudentsList.SelectedIndex = -1;
+            NameBox.Text = "";
+            EmailBox.Text = "";
+            ProgramsBox.SelectedIndex = -1;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string name = NameBox.Text;
+            string email = EmailBox.Text;
+            Major selectedProgram = (Major)ProgramsBox.SelectedItem;
+            int programID = selectedProgram.MajorID;
+            string sql = "";
+
+            using (SqlConnection myConnection = new SqlConnection(connectionString))
+            {
+                myConnection.Open();
+
+                if (currentStudent == null)
+                {
+                    sql = "INSERT INTO students (name, email, programID) VALUES (@name, @mail, @progId)";
+                }
+                else
+                {
+                    sql = "UPDATE students SET name=@name, email=@mail, programID=@progId WHERE studentID=@studId";
+                }
+
+                using (SqlCommand myCommand = new SqlCommand(sql, myConnection))
+                {
+                    myCommand.Parameters.AddWithValue("@name", name);
+                    myCommand.Parameters.AddWithValue("@mail", email);
+                    myCommand.Parameters.AddWithValue("@progId", programID);
+                    if (currentStudent != null)
+                    {
+                        myCommand.Parameters.AddWithValue("@studId", currentStudent.StudentID);
+                    }
+
+                    myCommand.ExecuteNonQuery();
+                }
+
+                myConnection.Close();
+            }
+
+            ClearButton_Click(sender, e);
+            LoadData();
+        }
+
+        private void NameBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Major
+    {
+        public int MajorID { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int MajorID { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Email}, {MajorID})";
+        }
+    }
+}
